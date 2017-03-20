bae9352eca1de5cbc2e1bd9d59e3be62a383f4d4
MihailT <mikeslifix@gmail.com>
2017-03-20 08:58:18 +0500

    little fixes

---
+++ src/test/java/com/sdvor/messenger/managers/SmsCodeManagerTest.kt
@@ -0,0 +1,26 @@
+package com.sdvor.messenger.managers
+
+import org.junit.Ignore
+import org.junit.Test
+import kotlin.reflect.full.memberFunctions
+
+
+/**
+ * Created by mteterin on 16.03.2017.
+ */
+internal class SmsCodeManagerTest {
+
+    @Test
+    @Ignore
+    fun generateNumericCode_randomCode() {
+        val functions = SmsCodeManager::class.memberFunctions
+
+    }
+
+    @Test
+    @Ignore
+    fun checkCode() {
+
+    }
+
+}
